25b978a873934e3a66bc5ee45a80097e46b064f9
Darrin Scheive <dscheivecali@gmail.com>
2021-10-31 05:53:05 +0000

    Updated args and logic

--- light_flicker.py
+++ light_flicker.py
@@ -1,17 +1,22 @@
 #! /bin/python3
-# importing the requests library
 import requests
 import random
 import argparse
 import time
-from time import sleep
 
 parser = argparse.ArgumentParser(description='Script for controlling Home Assistant Light service to flicker a light')
-parser.add_argument('--ip', action="store", dest='ip')
-parser.add_argument('--key', action="store", dest='key')
-parser.add_argument('--entity', action="store", dest='entity')
-parser.add_argument('--duration', action="store", dest='duration')
-parser.add_argument('--brightness', action="store", dest='brightness')
+parser.add_argument('--ip', action="store", dest='ip',
+                    help='The ip of the Home Assistant Server')
+parser.add_argument('--key', action="store", dest='key',
+                    help='The api key for the user on the Home Assistant Server')
+parser.add_argument('--entity', action="store", dest='entity',
+                    help='The entity name for the light that will be affected(e.g light.kitchen_1)')
+parser.add_argument('--duration', action="store", dest='duration',
+                    help='The duration in seconds (e.g 60)')
+parser.add_argument('--brightness', action="store", dest='brightness',
+                    help='An integer for the brightness percentage with 100 as max brightness')
+parser.add_argument('--debug', action="store_true", dest='debug',
+                    help='Enables debug output. Used when run from the command line to see values')
 args = parser.parse_args()
 
 
@@ -19,36 +24,43 @@ args = parser.parse_args()
 on_api_endpoint = "http://" + args.ip + ":8123/api/services/light/turn_on"
 off_api_endpoint = "http://" + args.ip + ":8123/api/services/light/turn_off"
 
-    
+# Header info
 header = {'Content-Type': 'application/json',
           'Accept': 'application/json',
           'Authorization': 'Bearer ' + args.key}
 
 # data to be sent to api
 off_data = {"entity_id": str(args.entity)}
-on_data = {"entity_id": str(args.entity),
-           "brightness_pct": int(args.brightness)}
+if args.brightness == "":
+    on_data = {"entity_id": str(args.entity),
+               "brightness_pct": int(args.brightness)}
+else:
+    on_data = {"entity_id": str(args.entity)}
 
+# Debug Function
 def debug():
-    if debug == True:
-        print(response)
-        print("r=" + str(red) + ",g=" + str(green) + ",b=" + str(blue)) 
+    if args.debug == True:
+        print(response.json())
         print("sleep_int=" + str(sleep_int))
         print(timer)
 
+# Main loop args and loop
 now = time.time()
 timer = 0
 while timer <= int(args.duration):
+    # Off-to-on flicker logic
     sleep_int = round(random.uniform(0.00, .10), 2)
     response = requests.post(off_api_endpoint, json=off_data, headers=header)
     debug()
-    sleep(sleep_int)
+    time.sleep(sleep_int)
 
+    # On-to-off flicker logic
     sleep_int = round(random.uniform(0.00, 1.50), 2)
     response = requests.post(on_api_endpoint, json=on_data, headers=header)
     debug()
-    sleep(sleep_int)
+    time.sleep(sleep_int)
 
+    # Duration time logic
     end = time.time()
     timer = round(end - now)
     debug()
